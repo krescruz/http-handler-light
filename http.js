9d71391899a54e2dd54ec1fe8bf283afdae81f3e
Cresencio cruz <krescruz@gmail.com>
2017-09-07 12:16:59 -0500

    Http handler

---
+++ http.js
@@ -0,0 +1,50 @@
+/**
+ * Base module for the Http request handler
+ * http.js
+ *
+ * 2017 @krescruz http://github.com/krescruz
+ * @license http://opensource.org/licenses/MIT  MIT License
+ */
+
+(function($) {
+
+    var CONTENT_TYPE_DEFAULT = 'application/json; charset=utf-8';
+    var METHODS = {
+        'GET': 'GET',
+        'POST': 'POST',
+    };
+
+    function service() {
+        return {
+            'get': httpGet,
+            'post': httpPost,
+            'put': httpPut,
+            'path': httpPatch,
+            'delete': httpDelete,
+        };
+    }
+
+    function httpGet(url, data) {
+        return ajax({ 'url': url, 'data': data, 'method': METHODS.GET });
+    }
+
+    function httpPost(url) {
+        return ajax({ 'url': url, 'data': data, 'method': METHODS.GET });
+    }
+
+    function httpPut() {}
+
+    function httpPatch() {}
+
+    function httpDelete() {}
+
+    function ajax(config) {
+        return $.ajax({
+            'url': config.url,
+            'method': config.method,
+            'data': config.data,
+            'contentType': CONTENT_TYPE_DEFAULT
+        });
+    }
+
+})(jQuery);
